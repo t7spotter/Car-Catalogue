e0111af2ebf835ec0a38d39b3a74618f02a8db29
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-24 17:08:33 +0330

    Added Company model to the models.py

--- catalouge/models.py
+++ catalouge/models.py
@@ -6,3 +6,10 @@ class Category(models.Model):
     def __str__(self):
         return self.title
     
+class Company(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
+    
+    
