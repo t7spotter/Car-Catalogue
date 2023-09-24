06983a3d58e389ecab362723071f04b1d95a05de
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-24 20:49:37 +0330

    Added db_table to the category table

--- catalouge/models.py
+++ catalouge/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=128)
     
+    class Meta:
+        db_table = "Category"
     def __str__(self):
         return self.title
     
