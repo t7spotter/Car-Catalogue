f623388d1d3eb5416fa605b846e69d650f1ba8ed
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-26 19:51:52 +0330

    Represented search by query params for ListCreateCompany view

--- catalouge/views.py
+++ catalouge/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Car, Company, Category
 from .serializers import CarSerializer, CompanySerializer, CategorySerializer
@@ -7,6 +8,8 @@ from .serializers import CarSerializer, CompanySerializer, CategorySerializer
 class ListCreateCompany(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["name",]
     
     
 class RetrieveCompany(generics.RetrieveUpdateDestroyAPIView):
